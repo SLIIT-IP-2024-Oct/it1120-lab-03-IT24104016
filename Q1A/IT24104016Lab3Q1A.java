2574c95529d80cc2247f9bd4719ccf4e2b8d6ff9
IT24104016 <IT24104016@my.sliit.lk>
2024-12-13 14:41:28 +0530

    Add files via upload

---
+++ Q1A/IT24104016Lab3Q1A.java
@@ -0,0 +1,9 @@
+public class IT24104016Lab2Q3 { 
+    public static void main(String[] args) {         double sideA = 3.0;          double sideB = 4.0;  
+ 	 	 
+        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB)); 
+         
+        System.out.println("Length of the hypotenuse: " + hypotenuse); 
+    } 
+} 
+ 
